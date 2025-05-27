b022ff8c4d872b9b726ef02b8745942cb740009f
Spicy-Z <ishozaia22@gmail.com>
2025-05-26 20:47:50 -0400

    Functional changes to the JS and a README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock Paper Scissors
+
+A project from The Odin Project's foundational course, includes very basic game with no front-end design implemented in order to create a simple yet functional game of rock paper scissors with a computer.
--- index.js
+++ index.js
@@ -1,65 +1,61 @@
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 100 + 1)
-    let choice
-    if (number <= 33) {
-        choice = "Rock"
-    } else if (number > 33 && number <= 66) {
-        choice = "Paper"
-    } else if (number > 66 && number <= 100) {
-        choice = "Sissors"
-    };
-    return choice;
+  let number = Math.floor(Math.random() * 100 + 1);
+  let choice;
+  if (number <= 33) {
+    choice = "Rock";
+  } else if (number > 33 && number <= 66) {
+    choice = "Paper";
+  } else if (number > 66 && number <= 100) {
+    choice = "Sissors";
+  }
+  return choice;
 }
 
 function chosenValues(human, computer) {
-    let player = document.querySelector('#playerSelect')
-    let comp = document.querySelector('#computerSelect')
+  let player = document.querySelector("#playerSelect");
+  let comp = document.querySelector("#computerSelect");
 
-    player.textContent = `You selected: ${human}`
-    comp.textContent = `Computer selected: ${computer}`
+  player.textContent = `You selected: ${human}`;
+  comp.textContent = `Computer selected: ${computer}`;
 }
 
 function playRound(humanChoice, computerChoice) {
-    let winner
+  let winner;
 
-    chosenValues(humanChoice, computerChoice)
+  chosenValues(humanChoice, computerChoice);
 
-    if (computerChoice == "Paper" && humanChoice == "Rock" ||
-        computerChoice == "Sissors" && humanChoice == "Paper" ||
-        computerChoice == "Rock" && humanChoice == "Sissors"
-    ) {
-        winner = 0;
-    } else if (computerChoice == humanChoice) {
-    } else {
-        winner = 1;
-    }
-    return winner;
+  if (
+    (computerChoice == "Paper" && humanChoice == "Rock") ||
+    (computerChoice == "Sissors" && humanChoice == "Paper") ||
+    (computerChoice == "Rock" && humanChoice == "Sissors")
+  ) {
+    winner = 0;
+  } else if (computerChoice == humanChoice) {
+  } else {
+    winner = 1;
+  }
+  return winner;
 }
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let winner;
+  let humanScore = 0;
+  let computerScore = 0;
+  let winner;
 
-    let selections = document.querySelector('#selectors')
-    let score = document.querySelector('#scores')
-    let win = document.querySelector('#winner')
+  let selections = document.querySelector("#selectors");
+  let score = document.querySelector("#scores");
+  let win = document.querySelector("#winner");
 
-    selections.addEventListener('click', (e) => {
-        if (humanScore >= 5 || computerScore >= 5) {
-            win.textContent = humanScore > computerScore ? 'You Win!' : 'You Lose :('
-            e.target.preventDefault();
-            
-        }
-        let humanChoice = e.target.id;
-        winner = playRound(humanChoice, getComputerChoice())
-
-
+  selections.addEventListener("click", (e) => {
+    score.textContent = `Your Score: ${humanScore} \nComputer Score: ${computerScore}`;
+    if (humanScore == 5 || computerScore == 5) {
+      win.textContent = humanScore > computerScore ? "You Win!" : "You Lose :(";
+      e.target.preventDefault();
+    }
+    let humanChoice = e.target.id;
+    winner = playRound(humanChoice, getComputerChoice());
     winner == 1 ? humanScore++ : computerScore++;
-    score.textContent = `Your Score: ${humanScore} \nComputer Score: ${computerScore}`
-    })
-
+  });
 }
-
 
 playGame();
